f0f56de317349619c281473d349f9c977beafbbf
Rado <radovid@users.noreply.github.com>
2019-01-19 01:53:28 +0200

    Update DecipherTitleChange.user.js

--- DecipherTitleChange.user.js
+++ DecipherTitleChange.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name      Decipher Title Changer
 // @namespace https://github.com/radovid/decipherTitlesScript
-// @version   0.6
+// @version   0.7
 // @description  Userscript for changing webpage titles (tab names) for decipher surveys to include Mac/SN
 // @updateURL https://github.com/radovid/decipherTitlesScript/raw/master/DecipherTitleChange.user.js
 // @include https://surveys.globaltestmarket.com/*
@@ -22,6 +22,7 @@ var walmart = /\/53c\/([0-9]{2,}[0-9A-Za-z\_]+)/;
 var maps = /gmi\/v2\/maps\/([0-9]{2,}[0-9A-Za-z\_]+)/;
 var natov3 = /gmi\/v3\/NATO\/AMS\/([0-9]{2,}[0-9A-Za-z\_]+)/;
 var natov2 = /gmi\/v2\/NATO\/([0-9]{2,}[0-9A-Za-z\_]+)/;
+var internalv3 = /gmi\/v3\/(?:[A-Z]+)\/INTERNAL\/([0-9]{2,}[0-9A-Za-z\_]+)/;
 
 var currTitle = '';
 if (document.title.includes("error")) {
@@ -31,8 +32,8 @@ if (document.title.includes("error")) {
 }
 
 //var prjTitle = document.getElementsByClassName("title-1")[0].innerText;
-var dirs = [v3,v2,gmi,kantar3,kantar2,ag,walmart,maps,natov3,natov2];
-var dirNames = ['v3/','v2/','gmi/','ktr-v3/','ktr-v2/','AG/','wmt/','wmt/','Nato/','GBHT/'];
+var dirs = [v3,v2,gmi,kantar3,kantar2,ag,walmart,maps,natov3,natov2,internalv3];
+var dirNames = ['v3/','v2/','gmi/','ktr-v3/','ktr-v2/','AG/','wmt/','wmt/','Nato/','GBHT/','v3/INT/'];
 
 setTimeout( function() {
   for (var i = 0; i < dirs.length; i++) {
