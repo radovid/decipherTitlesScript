bf2a2749113f5ae5f23796a751e8d38758be2583
Rado <radovid@users.noreply.github.com>
2019-01-21 16:14:43 +0100

    Fix for dynamic pages
    Some pages don't reload when changed and the script doesn't run
    Some code order

--- DecipherTitleChange.user.js
+++ DecipherTitleChange.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          Decipher Title Changer
 // @namespace     https://github.com/radovid/decipherTitlesScript
-// @version       1.3
+// @version       1.4
 // @description   Userscript for changing webpage titles (tab names) for decipher surveys to include Mac/SN
 // @downloadURL https://github.com/radovid/decipherTitlesScript/raw/master/DecipherTitleChange.user.js
 // @updateURL https://github.com/radovid/decipherTitlesScript/raw/master/DecipherTitleChange.user.js
@@ -11,7 +11,7 @@
 // @include http://*.decipherinc.com/*
 // ==/UserScript==
 
-var url = location.href; // Get current url
+
 // Set regexp for different paths
 var v3 = /gmi\/v3\/(?:[A-Z]+)\/([0-9]{2,}[0-9A-Za-z\_]+)/;
 var v2 = /gmi\/v2\/([0-9]{2,}[0-9A-Za-z\_]+)/;
@@ -25,83 +25,98 @@ var natov3 = /gmi\/v3\/NATO\/AMS\/([0-9]{2,}[0-9A-Za-z\_]+)/;
 var natov2 = /gmi\/v2\/NATO\/([0-9]{2,}[0-9A-Za-z\_]+)/;
 var internalv3 = /gmi\/v3\/(?:[A-Z]+)\/INTERNAL\/([0-9]{2,}[0-9A-Za-z\_]+)/;
 
-var title = '';
-var currTitle = document.title;
-// Get appropriate name for portal page
-if (title.includes("error")) {
-  title = currTitle;
-}
-else if (url.includes("/apps/report/")) {
-  if (url.includes("/edit/")) {
-    title = "Edit Crosstab";
-  } else if (url.includes("#!/report/")) {
-    title = "Crosstab Run";
-  } else {
-    title = "Crosstabs";
-  }
-}
-else if (url.includes("/apps/dashboard/")) {
-  if (url.includes(":edit")) {
-    title = "Edit Dashboard";
-  } else if (url.includes(":view")) {
-    title = currTitle;
-  } else {
-    title = "Dashboards";
-  }
-}
-else if (url.includes("/apps/respondents/")) {
-  title = "View/Edit Responses";
-}
-else if (url.includes("/apps/takesurvey/")) {
-  title = "Test Survey";
-}
-else if (url.includes("?config=GmGrUA&run=1")) {
-  title = "Download Data";
-}
-else if (url.includes("?markers=1")) {
-  title = "Markers";
-}
-else if (url.includes(":vars")) {
-  title = "Variables";
-}
-else if (url.includes(":edit")) {
-  title = "Edit Data";
-}
-else if (url.includes("admin/sst/list")) {
-  title = "Test Data History";
-}
-else if (url.includes("admin/sst/sst")) {
-  title = "Run Test Data / check for errors";
-}
-else if (url.includes("admin/vc/list")) {
-  title = "Change History";
-}
-else if (url.includes("/projects/detail")) {
-  title = "Project Details - Portal";
-}
-else {
-  title = currTitle.split(' ')[0];
-}
 
 //var prjTitle = document.getElementsByClassName("title-1")[0].innerText;
 var dirs = [v3,v2,gmi,kantar3,kantar2,ag,walmart,maps,natov3,natov2,internalv3];
 var dirNames = ['v3/','v2/','gmi/','ktr-v3/','ktr-v2/','AG/','wmt/','wmt/','Nato/','GBHT/','v3/INT/'];
 
-// On ome pages title is set with JS and timeout is needed so our set doesn't get overwritten
-setTimeout( function() {
+
+function setTitle() {
+  var url = location.href; // Get current url
+  var title = '';
+  var currTitle = document.title; // Get default title
+
+  // Set appropriate name for portal page
+  if (title.includes("error")) {
+    title = currTitle;
+  }
+  else if (url.includes("/apps/report/")) {
+    if (url.includes("/edit/")) {
+      title = "Edit Crosstab";
+    } else if (url.includes("#!/report/")) {
+      title = "Crosstab Run";
+    } else {
+      title = "Crosstabs";
+    }
+  }
+  else if (url.includes("/apps/dashboard/")) {
+    if (url.includes(":edit")) {
+      title = "Edit Dashboard";
+    } else if (url.includes(":view")) {
+      title = currTitle;
+    } else {
+      title = "Dashboards";
+    }
+  }
+  else if (url.includes("/apps/respondents/")) {
+    title = "View/Edit Responses";
+  }
+  else if (url.includes("/apps/takesurvey/")) {
+    title = "Test Survey";
+  }
+  else if (url.includes("?config=GmGrUA&run=1")) {
+    title = "Download Data";
+  }
+  else if (url.includes("?markers=1")) {
+    title = "Markers";
+  }
+  else if (url.includes(":vars")) {
+    title = "Variables";
+  }
+  else if (url.includes(":edit")) {
+    title = "Edit Data";
+  }
+  else if (url.includes("admin/sst/list")) {
+    title = "Test Data History";
+  }
+  else if (url.includes("admin/sst/sst")) {
+    title = "Run Test Data / check for errors";
+  }
+  else if (url.includes("admin/vc/list")) {
+    title = "Change History";
+  }
+  else if (url.includes("/projects/detail")) {
+    title = "Project Details - Portal";
+  }
+  else {
+    title = currTitle.split(' ')[0];
+  }
+
+  // Match regexps and set new title
   for (var i = 0; i < dirs.length; i++) {
     if (url.search(dirs[i]) > 0) {
       var studyNum = url.match(dirs[i])[1];
       /*if (currTitle.includes(studyNum)) {
-        title += "  (" + currTitle.split(" for ")[1] + ")";
+          title += "  (" + currTitle.split(" for ")[1] + ")";
       }*/
       // Check for and add temp to title
       if (url.includes("temp-")) {
         document.title = dirNames[i] + studyNum + '/temp: ' + title;
       } else {
-        document.title = dirNames[i] + studyNum + ': ' + title;
+          document.title = dirNames[i] + studyNum + ': ' + title;
       }
       break;
     }
   }
-}, 200)
+}
+
+// On some pages title is set with JS and timeout is needed so our set doesn't get overwritten
+setTimeout( function() {
+    setTitle()
+}, 200);
+
+// Sometimes page isn't reloaded when changed and seTitle won't run, hashchange fixes this
+window.addEventListener('hashchange', function(){
+    setTitle();
+});
+
